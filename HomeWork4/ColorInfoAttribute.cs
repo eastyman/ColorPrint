84b309fce50e1e85d9b22a473a0552ec55a07557
eastyman <bra4o.il@gmail.com>
2015-12-15 18:58:31 +0200

    2

---
+++ HomeWork4/ColorInfoAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HomeWork4
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class ColorInfoAttribute:System.Attribute
+    {
+        public ConsoleColor Color { get; set; }
+        public ColorInfoAttribute()
+        { }
+        public ColorInfoAttribute(ConsoleColor color)
+        {
+            Color = color;
+        }
+        public override string ToString()
+        {
+            return Color.ToString();
+        }
+    }
+}
---
+++ HomeWork4/Colorizer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+
+namespace HomeWork4
+{
+    public class Colorizer
+    {
+        public static void ColorPrint(object obj)
+        {
+            Type t = obj.GetType();
+            object o = Activator.CreateInstance(t);
+            o = obj;
+            ConsoleColor color;
+            foreach (PropertyInfo p in t.GetProperties())
+            {
+                // Пытаемся получить значение атрибута
+                try
+                {
+                    color = ((ColorInfoAttribute)p.GetCustomAttribute(typeof(ColorInfoAttribute))).Color;
+                    //Если атрибут назначен, но цвет не указан, устанавливаем белый цвет
+                    if (color == ConsoleColor.Black)
+                    {
+                        color = ConsoleColor.White;
+                    }
+
+                }
+                // если атрибут не назначен - устанавливаем белый цвет
+                catch (Exception) 
+                {                    
+                    color = ConsoleColor.White;                    
+                }
+                Console.ForegroundColor = color;
+                Console.WriteLine(p.GetValue(o));
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+        }
+    }
+}
